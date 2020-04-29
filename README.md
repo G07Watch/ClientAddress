a4d1f565f9a2526e6914ae3090e5693fd16db190
Garvin <gming27@gmail.com>
2020-04-29 08:45:50 -0400

    updates location to obtain client lat/lng and makes request via Google Geolocation API

--- README.md
+++ README.md
@@ -10,5 +10,13 @@ After cloning to run the application, please do the following from a terminal wi
 2) 'npm run frontend-install'
   Once the frontend has completed building use
 
-3) 'npm run activate'
+4) Create a config folder at the root level.  Inside it place a keys.js file.
+
+5) Within the file export an object with a key of 'secretOrKey' whose value is a googleAPI key, in order for the API call to work correctly.  It should look similar in format to the following:
+
+  module.exports = {
+  secretOrKey: 'your-key-here'
+  }
+
+6) 'npm run activate'
 
--- routes/api/geo.js
+++ routes/api/geo.js
@@ -1,24 +1,55 @@
 const express = require('express');
 const router = express.Router();
+const key = require('../../config/keys').secretOrKey;
 
 const axios = require('axios');
 
-router.get('/', (req, res) => {
-    axios('https://geoip-db.com/json',
-        {
-          method: 'GET',
-          mode: 'no-cors',
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json',
-          },
-          withCredentials: true,
-          credentials: 'same-origin',
-        })
-        .then( local => { 
-          return res.json(local.data.city) })
-        .catch(error => error);
+router.get('/:lat/:lng', (req, res) => {
+  let lat = req.params.lat;
+  let lng = req.params.lng;
+
+  axios(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${key}`,
+    {
+      method: 'GET',
+      mode: 'no-cors',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+      withCredentials: true,
+      credentials: 'same-origin',
+    }
+  )
+  .then( response => {
+    let city = response.data.results[0].address_components[2].long_name;
+
+    return res.json(city);
+  })
+  .catch(error => {
+    return error;
+  });
 
 });
+
+// router.get('/', (req, res) => {
+//     axios('https://geoip-db.com/json',
+//         {
+//           method: 'GET',
+//           mode: 'no-cors',
+//           headers: {
+//             'Access-Control-Allow-Origin': '*',
+//             'Content-Type': 'application/json',
+//           },
+//           withCredentials: true,
+//           credentials: 'same-origin',
+//         })
+//         .then( local => { 
+//           return res.json(local.data.city) })
+//         .catch(error => error);
+
+// });
+
+
 
 module.exports = router;
